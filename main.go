2cad388d7bfc0556091c8a73372777f3d1fd4cce
Julia Savchenko <iusavchenko@dinord.ru>
2024-04-12 18:47:17 +0400

    added metrics and dockerfile

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"net/http"
+	"runtime"
+	"runtime/metrics"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var PORT = ":1234"
+
+var n_goroutines = prometheus.NewGauge(prometheus.GaugeOpts{
+	Namespace: "packt",
+	Name:      "n_goroutines",
+	Help:      "Number of goroutines",
+})
+
+var n_memory = prometheus.NewGauge(prometheus.GaugeOpts{
+	Namespace: "packt",
+	Name:      "n_memory",
+	Help:      "Memory usage",
+})
+
+func main() {
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	prometheus.MustRegister(n_goroutines)
+	prometheus.MustRegister(n_memory)
+
+	const nGo = "/sched/goroutines:goroutines"
+	const nMem = "/memory/classes/heap/free:bytes"
+
+	getMetric := make([]metrics.Sample, 2)
+	getMetric[0].Name = nGo
+	getMetric[1].Name = nMem
+
+	http.Handle("/metrics", promhttp.Handler())
+
+
+	go func ()  {
+		for i := 0; i < 4; i++ {
+			go func() {
+				_ = make([]int, 1000000)
+				time.Sleep(time.Duration(r.Intn(10)) * time.Second)
+			}()
+		}
+
+		runtime.GC()
+		metrics.Read(getMetric)
+		goVal := getMetric[0].Value.Uint64()
+		goMem := getMetric[1].Value.Uint64()
+
+		time.Sleep(time.Duration(r.Intn(15)) * time.Second)
+		n_goroutines.Set(float64(goVal))
+		n_memory.Set(float64(goMem))
+	}()
+		log.Println("Listening to port", PORT)
+		log.Println(http.ListenAndServe(PORT, nil))
+
+}
